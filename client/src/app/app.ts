25c340a7d08be691c4a9fc72d96f9cbfc1018cd8
Muniz Neto <muniznetto@outlook.com>
2025-10-03 16:10:26 -0300

    feat: add `bold` formatting button

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -4,8 +4,11 @@ import { Component, signal } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('client');
+  format(command: string) {
+    document.execCommand(command, false, undefined);
+  }
 }
