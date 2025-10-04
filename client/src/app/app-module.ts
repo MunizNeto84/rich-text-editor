ac09d624cffd14181d71faf2b43222ce7c3137e5
Muniz Neto <muniznetto@outlook.com>
2025-10-04 08:33:52 -0300

    feat: connect `frontend` client to `backend` API

--- client/src/app/app-module.ts
+++ client/src/app/app-module.ts
@@ -1,11 +1,12 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { App } from './app';
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
